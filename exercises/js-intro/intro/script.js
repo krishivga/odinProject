9aa48db5fd75bfda29f329c006a112e39e6248c6
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-07-30 14:52:22 +0530

    Did some basic JS in Intro directory

---
+++ exercises/js-intro/intro/script.js
@@ -0,0 +1,23 @@
+console.log(55 + 33);
+console.log("-------------");
+
+console.log(5 + 3 + 10 + 8 + 224 + 5);
+console.log("--------------");
+
+console.log((4 + 6 + 9) / 77);
+console.log("--------------");
+
+// Moving onto variables
+let a = 10;
+console.log(a + 5);
+console.log(9*a);
+let b = 7 * a;
+console.log(b)
+console.log("---------------")
+
+// Constants
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual/MAX;
+console.log(percentage);
+
