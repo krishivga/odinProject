a5f97014829db2e9a388714e3cf5a9c634e3a293
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-08-03 09:34:29 +0530

    Finished fourth JS exercise about removing items from an array
    Used the fitler function for a one line solution.
    Used ...args object to utilise remaining arguments in a function. JS is definitely more complex but way more useful than python

---
+++ exercises/js/04_removeFromArray/removeFromArray.js
@@ -0,0 +1,7 @@
+const removeFromArray = function(array, ...items) {
+  return array.filter(item => !items.includes(item));
+}
+  
+
+// Do not edit below this line
+module.exports = removeFromArray;
