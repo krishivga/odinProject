0e7ece5ae470d28d40ba94e4389a8d1da84aeaa8
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-08-04 19:41:57 +0530

    Added dom elements and understood basic DOM workflow

--- foundations/js_dom_practice/basic_practice/index.js
+++ foundations/js_dom_practice/basic_practice/index.js
@@ -1,13 +1,38 @@
 // your JavaScript file
+// Main container managing DOM items
 const container = document.querySelector("#container");
 
+// First div containing text content
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
-
 container.appendChild(content);
 
+// A red paragraph
 const paragraph_red = document.createElement("p");
 paragraph_red.textContent = "Hey, I'm red!";
 paragraph_red.setAttribute("style", "color: red;")
 container.appendChild(paragraph_red);
+
+// A blue header
+const header_blue = document.createElement("h3");
+header_blue.setAttribute("style", "color: blue;");
+header_blue.textContent = "I'm a blue H3!";
+container.appendChild(header_blue);
+
+// A pink div containing content
+const div_pink = document.createElement("div");
+div_pink.setAttribute("style", "border: 1px solid black; background-color: pink;");
+
+// A header for the div
+const header_div = document.createElement("h1");
+header_div.textContent = "I'm in a div!";
+div_pink.appendChild(header_div);
+
+// A paragraph for the div
+const paragraph_div = document.createElement("p");
+paragraph_div.textContent = "ME TOO!";
+div_pink.appendChild(paragraph_div);
+
+// Adding the div to the container
+container.appendChild(div_pink)
