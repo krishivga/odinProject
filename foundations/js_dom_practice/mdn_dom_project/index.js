9e34467442d591324ad26a134bcbfcf222032924
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-08-08 19:55:34 +0530

    Finished MDN exercise

--- foundations/js_dom_practice/mdn_dom_project/index.js
+++ foundations/js_dom_practice/mdn_dom_project/index.js
@@ -4,13 +4,6 @@ const user_text_input = document.querySelector("input");
 const add_item_button = document.querySelector("button");
 let item_name;
 
-// Event listner for add_item_button
-add_item_button.addEventListener("click", function(){
-    item_name = document.getElementById("item").value;
-    console.log(item_name);
-    document.getElementById("item").value = "";
-});
-
 // Creating 3 elements and storign them in variables
 const created_list_item = document.createElement("li");
 const created_span = document.createElement("span");
@@ -18,3 +11,19 @@ const created_button = document.createElement("button");
 
 created_list_item.appendChild(created_span);
 created_list_item.appendChild(created_button);
+
+// Event listner for add_item_button
+add_item_button.addEventListener("click", function(){
+    item_name = user_text_input.value;
+    created_span.textContent = item_name;
+    created_button.textContent = "Delete";
+    console.log(item_name);
+    user_text_input.value = "";
+});
+
+list_of_items.appendChild(created_list_item);
+
+created_button.addEventListener("click",function() {
+    list_of_items.removeChild(created_list_item);
+})
+user_text_input.focus();
