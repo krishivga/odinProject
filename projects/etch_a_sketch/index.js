0da7597cdfdf68d5636212819b8979d36943001a
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-08-09 18:28:41 +0530

    Squares now turn black on mouseover Added array format for cells, need to refactor to build the set of cells from that and then append to container. Used forEach to track the same event across all elements

--- projects/etch_a_sketch/index.js
+++ projects/etch_a_sketch/index.js
@@ -8,4 +8,9 @@ for (let divCount = 0; divCount < 256; divCount++) { // 16 x 16 = 256
     sketchContainer.appendChild(sketchCell);
 }
 
-
+let sketchCells = document.querySelectorAll(".cell"); // Array of all the cells
+sketchCells.forEach(function (cell) {
+    cell.addEventListener("mouseenter", function () {
+        cell.setAttribute("style", "background-color: black;");
+    })
+})
