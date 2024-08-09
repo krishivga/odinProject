523d0d08065f90f26add0666eb4dd54089f44f2e
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-08-09 17:59:32 +0530

    .

--- projects/etch_a_sketch/index.js
+++ projects/etch_a_sketch/index.js
@@ -1,8 +1,11 @@
 const sketchContainer = document.querySelector(".container");
 let sketchCell;
 
+// Creating 256 divs to be used as cells
 for (let divCount = 0; divCount < 256; divCount++) { // 16 x 16 = 256
     sketchCell = document.createElement("div"); // appendChild removes value from original location, need to make a new one for each iteration of loop
     sketchCell.setAttribute("class", "cell");
     sketchContainer.appendChild(sketchCell);
 }
+
+
