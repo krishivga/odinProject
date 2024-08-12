a14ff248e35b719df24185b522a74d695a280d9a
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-08-12 16:16:31 +0530

    Finished JQQ practice.

--- foundations/js_basics_practice/array_worksheet_jqq/index.js
+++ foundations/js_basics_practice/array_worksheet_jqq/index.js
@@ -50,3 +50,28 @@ const map_heights_and_names = characters.map((character) => {
 // Array of just first names
 const map_first_names = characters.map((character) => (character.name).split(" ")[0]);
 // console.log(map_first_names);
+
+// REDUCE
+// Total mass of all characters
+const total_mass = characters.reduce((total, character) => {
+    return Number(character.mass) + Number(total) // To number as original value is string
+}, 0);
+//console.log(total_mass);
+
+// Total Height of all characters
+const total_height = characters.reduce((total, character) => {
+    return Number(character.height) + Number(total) // To number as original value is string
+}, 0);
+//console.log(total_height);
+
+// Total number of characters in each character name
+const total_characters = characters.reduce((total, character) => {
+    return Number(character.name.length) + total;
+}, 0);
+//console.log(total_characters);
+
+// Total number of characters in eye color
+const total_characters_in_eye = characters.reduce((total, character) => {
+    return Number(character.eye_color.length) + total;
+}, 0);
+//console.log(total_characters_in_eye);
